a486ef76f7c1a4dc6aac604a06978600bed75455
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-01 20:36:09 +0300

    completed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file));) {
+            String line;
+            while ((line =  reader.readLine()) != null){
+                String [] values = line.split(":");
+                String key = values[0].trim();
+                String value = values[1].trim();
+               switch (key){
+                   case ("Name"):
+                       profile.setName(value);
+                       break;
+                   case ("Age"):
+                       profile.setAge(Integer.parseInt(value));
+                       break;
+                   case ("Email"):
+                       profile.setEmail(value);
+                       break;
+                   case ("Phone"):
+                       profile.setPhone(Long.parseLong(value));
+                       break;
+               }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return profile;
     }
+
 }
