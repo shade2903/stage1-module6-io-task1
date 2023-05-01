f92a0b1d00dc9299a89567b3ebf634ab4244974e
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-01 20:47:19 +0300

    completed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -9,30 +9,35 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        Profile profile = new Profile();
+        Profile profile;
         try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file));) {
-            String line;
-            while ((line =  reader.readLine()) != null){
-                String [] values = line.split(":");
-                String key = values[0].trim();
-                String value = values[1].trim();
-               switch (key){
-                   case ("Name"):
-                       profile.setName(value);
-                       break;
-                   case ("Age"):
-                       profile.setAge(Integer.parseInt(value));
-                       break;
-                   case ("Email"):
-                       profile.setEmail(value);
-                       break;
-                   case ("Phone"):
-                       profile.setPhone(Long.parseLong(value));
-                       break;
-               }
-            }
+            profile = getValueFromLine(reader);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        return profile;
+    }
+    public Profile getValueFromLine(BufferedReader reader) throws IOException {
+        Profile profile = new Profile();
+        String line;
+        while ((line =  reader.readLine()) != null){
+            String [] values = line.split(":");
+            String key = values[0].trim();
+            String value = values[1].trim();
+            switch (key){
+                case ("Name"):
+                    profile.setName(value);
+                    break;
+                case ("Age"):
+                    profile.setAge(Integer.parseInt(value));
+                    break;
+                case ("Email"):
+                    profile.setEmail(value);
+                    break;
+                case ("Phone"):
+                    profile.setPhone(Long.parseLong(value));
+                    break;
+            }
         }
         return profile;
     }
